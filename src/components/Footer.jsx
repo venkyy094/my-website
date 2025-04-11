05c85b7533d2cf06e453d9cc66484df1172ca2db
venkyy094 <venkyy094@gmail.com>
2025-04-11 21:10:20 +0530

    Added new folders and updated content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,14 @@ import "./Footer.css";
 const Footer = () => (
   <footer className="footer">
     <div className="footer-content">
-      <p>Email: venkyy094@gmail.com</p>
-      <p>Phone: +91-9676821981</p>
+      <p>Email: venkatesh.270824@gmail.com</p>
+      <p>Phone: +91-9100586696</p>
       <p>
         GitHub:{" "}
         <a href="https://github.com/venkyy094" target="_blank" rel="noreferrer">
           github.com/venkyy094
         </a>
       </p>
-      
     </div>
   </footer>
 );
